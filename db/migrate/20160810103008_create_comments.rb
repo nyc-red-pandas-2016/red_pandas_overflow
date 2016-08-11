a66e70db2caaa8b3a74b473e4cacc0babc07e7da
Matthew Kubota & Walter Chabla <pair+mattk5.walterchabla@devbootcamp.com>
2016-08-11 15:20:53 -0400

    conflicts wiht users session fixed

--- db/migrate/20160810103008_create_comments.rb
+++ db/migrate/20160810103008_create_comments.rb
@@ -1,4 +1,4 @@
-bodclass CreateComments < ActiveRecord::Migration
+class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text    :body, null: false
