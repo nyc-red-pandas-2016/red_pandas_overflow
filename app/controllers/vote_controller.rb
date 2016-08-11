3c67e874db6036bee0938596025096704648e124
Matthew Kubota & Walter Chabla <pair+mattk5.walterchabla@devbootcamp.com>
2016-08-11 18:00:32 -0400

    begin working on ajax

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -3,22 +3,30 @@ get '/vote/:id/up' do
   vote = Vote.find_by(voteable_id: params[:id])
   user = Vote.find_by(voteable_id: params[:id])
 
-  if user == nil
-    if vote == nil
-      vote = answer.votes.create(vote_count: 1)
-      id = answer.question.id
-      redirect "/questions/#{id}"
+  if request.xhr?
+    if vote == nil && user == nil
+      vote = answer.votes.create(vote_count: 1).vote_count.to_s
     else
       up_vote = vote.vote_count + 1
-      vote.update_columns(vote_count: up_vote)
+      vote.update_columns(vote_count: up_vote).vote_count.to_s
+    end
+  else
+    if user == nil
+      if vote == nil
+        vote = answer.votes.create(vote_count: 1)
+        id = answer.question.id
+        redirect "/questions/#{id}"
+      else
+        up_vote = vote.vote_count + 1
+        vote.update_columns(vote_count: up_vote)
+        id = answer.question.id
+        redirect "/questions/#{id}"
+      end
+    else
       id = answer.question.id
       redirect "/questions/#{id}"
     end
-  else
-    id = answer.question.id
-    redirect "/questions/#{id}"
   end
-
 end
 
 get '/vote/:id/down' do
