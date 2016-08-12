56736e4da7ad0c053916c9f6bf0d0d59308f16b7
Elisa Verna <email": null>
2016-08-12 15:50:08 -0400

    added tag search function for questions

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -1,7 +1,14 @@
 get '/tags/show' do
+  @question_array = []
+
   tags = Tag.all.find_all do |tag|
            /#{params[:tag]}/ =~ tag.name
          end
-  
+
+  tags.each do |tag|
+    tag.taggable_type == "Question"
+    @question_array << Question.find(tag.taggable_id)
+  end
+
   erb :'/tags/show'
 end
