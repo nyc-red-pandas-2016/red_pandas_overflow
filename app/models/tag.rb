9990f7260908af5a205a6765ee951aed7d932329
Walter Chabla <walterchabla43@gmail.com>
2016-08-12 09:41:56 -0400

    deleted search tag method

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,10 +5,4 @@ class Tag < ActiveRecord::Base
   belongs_to :answer
 
   validates :name, presence: true
-
-  # def self.search_by_tag(tag_array, tag)
-  #   tag_array.find_all do |tag|
-  #     /#{tag}/ =~ tag.name
-  #   end
-  # end
 end
