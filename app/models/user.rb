a9eb4a521a62ceed8cc96be237d163ecf8350ae7
Elisa Verna <email": null>
2016-08-12 11:48:45 -0400

    create custom validation for email address format

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,17 @@ class User < ActiveRecord::Base
   validate :username_not_cat
   validates :password, length: { minimum: 6 }
   validates :email, presence: true, uniqueness: true
+  validate :email_format
 
   def username_not_cat
     if !username.include?("cat") && !username.include?("meow")
       errors.add(:username, " must contain the word 'cat' or 'meow'.")
+    end
+  end
+
+  def email_format
+    if !email.include?("@") && !email.include?(".com")
+      errors.add(:email, " must be in the proper format.")
     end
   end
 end
