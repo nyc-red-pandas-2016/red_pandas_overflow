b69d96fad1bca2561f9c2a6d2c0f03d561dd4ffd
Walter Chabla <walterchabla43@gmail.com>
2016-08-12 09:40:43 -0400

    working on the search bar

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,5 +4,11 @@ class Tag < ActiveRecord::Base
   belongs_to :question
   belongs_to :answer
 
-   validates :name, presence: true
+  validates :name, presence: true
+
+  # def self.search_by_tag(tag_array, tag)
+  #   tag_array.find_all do |tag|
+  #     /#{tag}/ =~ tag.name
+  #   end
+  # end
 end
