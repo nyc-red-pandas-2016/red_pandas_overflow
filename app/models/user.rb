b64528ed619d4693dc0a9796a9178f8c9b622f10
Elisa Verna <email": null>
2016-08-12 11:44:35 -0400

    finished username validation for word cat or meow

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,15 @@ class User < ActiveRecord::Base
 
   has_many :questions
   has_many :comments
-  has_many :votes, as: :voteables
+  has_many :votes, as: :voteable
   validates :username, presence: true, uniqueness: true
-  validated_format_of :username, :with => /(meow|cat)/i, :on => :create
+  validate :username_not_cat
   validates :password, length: { minimum: 6 }
   validates :email, presence: true, uniqueness: true
 
+  def username_not_cat
+    if !username.include?("cat") && !username.include?("meow")
+      errors.add(:username, " must contain the word 'cat' or 'meow'.")
+    end
+  end
 end
