17d6ab865721844a066d4d1ed0ca071c3e35e45c
Elisa Verna <email": null>
2016-08-12 10:53:58 -0400

    created validation for username creation, must include word meow or cat

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :votes, as: :voteables
   validates :username, presence: true, uniqueness: true
+  validated_format_of :username, :with => /(meow|cat)/i, :on => :create
   validates :password, length: { minimum: 6 }
   validates :email, presence: true, uniqueness: true
 
